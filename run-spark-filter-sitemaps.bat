fc131e54e528524a583da06c89270aa079f22b3d
Yehor Smoliakov <yehors@ukr.net>
2026-05-21 22:11:29 +0300

    Add files via upload

---
+++ run-spark-filter-sitemaps.bat
@@ -0,0 +1,7 @@
+@echo off
+
+set "SPARK_JAVA_OPTS=--add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/java.lang.invoke=ALL-UNNAMED --add-opens=java.base/java.lang.reflect=ALL-UNNAMED --add-opens=java.base/java.io=ALL-UNNAMED --add-opens=java.base/java.net=ALL-UNNAMED --add-opens=java.base/java.nio=ALL-UNNAMED --add-opens=java.base/java.util=ALL-UNNAMED --add-opens=java.base/java.util.concurrent=ALL-UNNAMED --add-opens=java.base/java.util.concurrent.atomic=ALL-UNNAMED --add-opens=java.base/jdk.internal.ref=ALL-UNNAMED --add-opens=java.base/sun.nio.ch=ALL-UNNAMED --add-opens=java.base/sun.nio.cs=ALL-UNNAMED --add-opens=java.base/sun.security.action=ALL-UNNAMED --add-opens=java.base/sun.util.calendar=ALL-UNNAMED --add-opens=java.security.jgss/sun.security.krb5=ALL-UNNAMED"
+
+java -Xmx4g %SPARK_JAVA_OPTS% -jar .\cc-robots-download-spark-v0.1.0.jar filter-sitemaps target/commoncrawl-sitemaps target/filtered-sitemaps local[10]
+
+pause
